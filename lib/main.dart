fcd7618102f1a01d94be9676e41c4294ba83456f
risingPhoenix7 <achinthyahebbar@gmail.com>
2022-03-17 10:31:35 +0530

    added bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,15 @@ class Home extends StatelessWidget {
             ),
             actions: <Widget>[
               IconButton(
-                onPressed: () {print('add a story');},
+                onPressed: () {
+                  print('add a story');
+                },
                 icon: Icon(Icons.add_box_outlined),
               ),
               IconButton(
-                  onPressed: () {print('open messenger');},
+                  onPressed: () {
+                    print('open messenger');
+                  },
                   icon: Image.asset(
                     'icons/messengerr.jpg',
                     height: 30,
@@ -40,271 +44,214 @@ class Home extends StatelessWidget {
                   )),
             ],
           ),
+          //for scrolling wrap with this
           body: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+                    Widget>[
+              SizedBox(height: 10),
+              //First row containing stories
+              Row(
+                children: <Widget>[
+                  Column(
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          print('Show the story');
+                        }, //Show the story
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                        ),
 
-                SizedBox(height: 10),
-                //First row containing stories
-                Row(
-                  children: <Widget>[
-                    Column(
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            print('Show the story');
-                          }, //Show the story
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all<Color>(Colors.black),
-                          ),
-
+                        child: CircleAvatar(
+                            radius: 30,
+                            backgroundImage:
+                                AssetImage('images/instagrambackground.jpg'),
+                            child: CircleAvatar(
+                                radius: 27,
+                                backgroundColor: Colors.black,
+                                child: CircleAvatar(
+                                  radius: 24,
+                                  backgroundImage:
+                                      AssetImage('icons/icon1.png'),
+                                ))),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        'memes',
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              //First line of post
+              Row(children: <Widget>[
+                ElevatedButton.icon(
+                  onPressed: () {
+                    print('Open the profile');
+                  },
+                  style: ElevatedButton.styleFrom(primary: Colors.black),
+                  icon: CircleAvatar(
+                      radius: 20,
+                      backgroundImage:
+                          AssetImage('images/instagrambackground.jpg'),
+                      child: CircleAvatar(
+                          radius: 17,
+                          backgroundColor: Colors.black,
                           child: CircleAvatar(
-                              radius: 30,
-                              backgroundImage: NetworkImage(
-                                  'https://www.zilliondesigns.com/blog/wp-content/uploads/instagram.jpg'),
-                              child: CircleAvatar(
-                                  radius: 27,
-                                  backgroundColor: Colors.black,
-                                  child: CircleAvatar(
-                                    radius: 24,
-                                    backgroundImage: NetworkImage(
-                                        'https://styles.redditmedia.com/t5_12jk54/styles/profileIcon_snoo4a45a8a1-6897-429a-8046-f1d24899fe6f-headshot-f.png'),
-                                  ))),
-                        ),
-                        SizedBox(
-                          height: 2,
-                        ),
-                        Text(
-                          'memes',
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                  ],
+                            radius: 14,
+                            backgroundImage: AssetImage('icons/icon1.png'),
+                          ))),
+                  label: Text('memes',
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
                 ),
                 SizedBox(
-                  height: 10,
+                  width: 220,
                 ),
-                //First line of post
-                Row(
-                  children:<Widget>[
-                    ElevatedButton.icon(
-                      onPressed: (){
-                        print('Open the profile');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.black
-                      ),
-                      icon:CircleAvatar(
-                          radius: 20,
-                          backgroundImage: NetworkImage(
-                              'https://www.zilliondesigns.com/blog/wp-content/uploads/instagram.jpg'),
-                          child: CircleAvatar(
-                              radius: 17,
-                              backgroundColor: Colors.black,
-                              child: CircleAvatar(
-                                radius: 14,
-                                backgroundImage: NetworkImage(
-                                    'https://styles.redditmedia.com/t5_12jk54/styles/profileIcon_snoo4a45a8a1-6897-429a-8046-f1d24899fe6f-headshot-f.png'),
-                              ))),
-                      label:Text(
-                          'memes',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20
-                          )
-                      ),
-
-
-                    ),
-                    SizedBox(
-                      width: 220,
-                    ),
-
-                    IconButton(
-                      onPressed: () {print('more options');  },
-                      icon: Icon(
-                        Icons.more_vert,
-                        color: Colors.white,
-                      ),
-
-
-
-                    )
-
-
-
-                  ]
-
-                ),
-
-
-                //Actual post
-
-                  Image.network(
-                    'https://www.linkpicture.com/q/memess.png',
-                 ),
-                Row(
-                  children:<Widget>[
-                    IconButton(
-                      icon: Icon(
-                          Icons.favorite_border_outlined,
-                          color: Colors.white,
-                          size:35
-                      ),
-                      onPressed: ()
-                      {print('Add to favourite');  },
-
-                    ),
-                    IconButton(
-                      icon: Icon(
-                          Icons.comment_bank_outlined,
-                          color: Colors.white,
-                          size:35
-                      ),
-                      onPressed: ()
-                      {print('Comment');  },
-
-                    ),
-                    IconButton(
-                      icon: Icon(
-                          Icons.share,
-                          color: Colors.white,
-                          size:35
-                      ),
-                      onPressed: ()
-                      {print('Share');  },
-
-                    ),
-
-                    SizedBox(
-                    width: 210,
-                    ),
-                    IconButton(
-                      icon: Icon(
-                          Icons.bookmark_outline,
-                      color: Colors.white,
-                      size:35
-                      ),
-                      onPressed: ()
-                      {print('Bookmark clicked');  },
-
-                    )
-                  ]
-
-
-                ),
-
-              Text(
-                  '  1,16,360 likes',
-                  style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18
-              )),
-                Row(
-                  children: [
-                    Text(
-                        '  memes',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18
-                        )),
-                    Text(
-                        '  lol',
-                        style: TextStyle(
-                            color: Colors.white,
-
-                            fontSize: 18
-                        ))
-                  ],
-                ),
-                TextButton(
+                IconButton(
                   onPressed: () {
-                    print('View comments');
+                    print('more options');
                   },
-                  style: TextButton.styleFrom(
-                    fixedSize:Size.fromWidth(200),
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: Colors.white,
                   ),
-                  child: Text(
-                      'View all 404 comments',
+                )
+              ]),
+
+              //Actual post
+
+              Image.asset(
+                'images/memes.png',
+              ),
+              Row(children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.favorite_border_outlined,
+                      color: Colors.white, size: 35),
+                  onPressed: () {
+                    print('Add to favourite');
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.comment_bank_outlined,
+                      color: Colors.white, size: 35),
+                  onPressed: () {
+                    print('Comment');
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.share, color: Colors.white, size: 35),
+                  onPressed: () {
+                    print('Share');
+                  },
+                ),
+                SizedBox(
+                  width: 210,
+                ),
+                IconButton(
+                  icon: Icon(Icons.bookmark_outline,
+                      color: Colors.white, size: 35),
+                  onPressed: () {
+                    print('Bookmark clicked');
+                  },
+                )
+              ]),
+
+              Text('  1,16,360 likes',
+                  style: TextStyle(color: Colors.white, fontSize: 18)),
+              Row(
+                children: [
+                  Text('  memes',
                       style: TextStyle(
-                          color: Colors.grey,
-
-                          fontSize: 17
-                      )),
-
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18)),
+                  Text('  lol',
+                      style: TextStyle(color: Colors.white, fontSize: 18))
+                ],
+              ),
+              TextButton(
+                onPressed: () {
+                  print('View comments');
+                },
+                style: TextButton.styleFrom(
+                  fixedSize: Size.fromWidth(200),
                 ),
-                Row(
-                  children: <Widget>[
-                    ElevatedButton.icon(
-                      onPressed: (){
-                        print('Open commenting');
+                child: Text('View all 404 comments',
+                    style: TextStyle(color: Colors.grey, fontSize: 17)),
+              ),
+              Row(
+                children: <Widget>[
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      print('Open commenting');
+                    },
+                    style: ElevatedButton.styleFrom(primary: Colors.black),
+                    icon: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('icons/icon2.jpg'),
+                    ),
+                    label: Text('Add a comment...',
+                        style: TextStyle(color: Colors.grey, fontSize: 17)),
+                  ),
+                  SizedBox(width: 50),
+                  IconButton(
+                      onPressed: () {
+                        print('add heart icon to comment');
                       },
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.black
-                      ),
-                      icon:CircleAvatar(
-                          radius: 20,
-                        backgroundImage: NetworkImage('https://cheszy.com/img/default/reddit-default-0079D3.jpg'),
-                              ),
-                      label:Text(
-                          'Add a comment...',
-                          style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 17
-                          )
-                      ),
-
-
-                    ),
-                    SizedBox(
-                      width:50
-                    ),
-                 IconButton(onPressed: (){print('add heart icon to comment');},
-                        icon: Icon(
-                            Icons.favorite,
-                            color: Colors.red,
-                            size:20
-                        )
-
-                    ),
-                    IconButton(onPressed: (){print('add handshake  icon to comment');},
-                     icon: Icon(
-                   Icons.handshake_rounded,
-                    color: Color(0xFFcb8442),
-                       size:20
-                 )
-
-                ),
-                    IconButton(onPressed: (){print('add comment');},
-                        icon: Icon(
-                            Icons.add_circle,
-                            color: Colors.white,
-                            size:20
-                        )
-
-                    ),
-
-
-
-
-
-              ],
-            ),
-          ]
-          )
-          ,
-
+                      icon: Icon(Icons.favorite, color: Colors.red, size: 20)),
+                  IconButton(
+                      onPressed: () {
+                        print('add handshake  icon to comment');
+                      },
+                      icon: Icon(Icons.handshake_rounded,
+                          color: Color(0xFFcb8442), size: 20)),
+                  IconButton(
+                      onPressed: () {
+                        print('add comment');
+                      },
+                      icon: Icon(Icons.add_circle_outline,
+                          color: Colors.white, size: 20)),
+                ],
+              ),
+            ]),
           ),
-
-    )
-    );
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+              showUnselectedLabels: false,
+            backgroundColor: Colors.black,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white,
+            iconSize: 30,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined),
+              label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.search),
+                  label: 'Search'),
+              BottomNavigationBarItem(icon: Icon(Icons.video_library_outlined),
+                  label: 'Reels'),
+              BottomNavigationBarItem(icon: Icon(Icons.favorite_border_outlined),
+                  label: 'Activity'),
+              BottomNavigationBarItem(icon: CircleAvatar(
+                radius: 20,
+                backgroundImage: AssetImage('icons/icon2.jpg'),
+              ),
+                  label: 'My profile'),
+            ],
+          ),
+        ));
 
     //Scaffold is like a wrapper to lots of widgets like appBar.body.bottomNavigationBar
     //bottomSheet,drawer,;
